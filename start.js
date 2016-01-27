f1d7ee3e5764947fb438e5f3c9a448c633ac1d62
olnaz <hi7emh1gh@gmail.com>
2016-01-27 11:08:34 +0300

    update start.js

--- start.js
+++ start.js
@@ -1,10 +1,16 @@
 'use strict';
 
+const spawn = require('child_process').spawn;
+
 let app = require('./app');
 let parsers = require('./app/parsers');
 
 if (process.argv.slice(2)[0] === '-debug') 
   process.env.DEBUG = true;
+
+/**
+ * Starting App
+ */
 
 let params = {
   appId: ID, 
@@ -17,3 +23,12 @@ app
   .configure(params)
   .use(parsers)
   .start();
+
+/**
+ * Starting extentions (auto-status, auto-adding friends)
+ */
+
+spawn('sudo', ['pm2', 'start', 'autochecks.js', '--name', 'achks'], {
+  stdio: 'ignore', 
+  detached: true
+}).unref();
